0e8546f7ca717a7464a7272978605e6660f2ea12
Vasu <vasucp1207@gmail.com>
2023-04-14 16:31:17 +0530

    remove email

--- src/components/SignInOauth.tsx
+++ src/components/SignInOauth.tsx
@@ -88,15 +88,7 @@ export function SignInOauth({ account, theme }: {
           </div>
           <button>Sign in with Github</button>
         </div>
-        <div onClick={() => signInOauth('email')} className='relative flex justify-center p-2 items-center gap-3 h-10 shadow-3xl hover:shadow-2xl rounded-sm w-60 bg-[#db4537]'>
-          <div className='w-[24px] h-[24px] flex items-center justify-self-center left-4 absolute'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="#FFFFFF">
-              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
-              <path d="M0 0h24v24H0z" fill="none" />
-            </svg>
-          </div>
-          <button>Sign in with Email</button>
-        </div>
+        
         <div onClick={() => signInOauth('microsoft')} className='relative flex justify-center p-2 items-center gap-3 h-10 shadow-3xl hover:shadow-2xl rounded-sm w-60 bg-[#2f2f2f]'>
           <div className='w-[24px] h-[24px] flex items-center justify-self-center left-4 absolute'>
             <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><title>MS-SymbolLockup</title><rect x="1" y="1" width="9" height="9" fill="#f25022" /><rect x="1" y="11" width="9" height="9" fill="#00a4ef" /><rect x="11" y="1" width="9" height="9" fill="#7fba00" /><rect x="11" y="11" width="9" height="9" fill="#ffb900" /></svg>
